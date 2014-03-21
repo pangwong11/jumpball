3d50535056b74880c1a2b8b6823713659e6a4898
Aidan Wong <aidan.wong@MBA-107-OP-31.local>
2014-03-20 22:36:03 -0700

    Updated nba_season_stats_parser_4.py

--- bd_collect/nba_season_stats_parser_4.py
+++ bd_collect/nba_season_stats_parser_4.py
@@ -27,7 +27,7 @@ page1 = urllib2.urlopen("http://www.nba.com/%s/roster/%s" % (team, season ))
 f = csv.writer(open('season_%s_%s_players_height.csv' % (season, team), 'w'))
 
 # CSV file header
-f.writerow(["name" ,"ht", "wt", "team"])
+f.writerow(["team", "name" ,"ht", "wt"])
 
 def getStatsFromWeb(page):
     content = page.read()
@@ -39,26 +39,31 @@ def getStatsFromWeb(page):
     rows = table.findAll('tr')
 #    new_rows = string.replace(rows[0], ',', '\n')
     #new_rows = re.sub(',', '\n', rows[0])
-    converted_rows = unicode.join(u'\n', map(unicode, rows))
+    #converted_rows = unicode.join(u'\n', map(unicode, rows))
 #    print converted_rows
 
-    new_soup = BeautifulSoup(converted_rows)
-    new_rows = new_soup.findAll('td')    
-    print type(new_rows)
-    print new_rows
+#    print type(new_rows)
+#    print new_rows
+    #del new_rows[0]
+    #data = new_rows
 
-    for data in new_rows:
-        print type(data)
-        cells = data.findAll('td')
+
+    for line in rows:
+#        print line
+        if len(line.findAll('td')) == 0:
+          continue
+        cells = line.findAll('td')
 #        if len(cells) == 31
-        print type(cells)
-#        name = cells[1].find(text=True)
+        #print cells
+        name = cells[1].find(text=True)
+        ht=cells[3].find(text=True).replace("-", ".")
+        wt=cells[4].find(text=True)
 #        ht = cells[6].find(text=True).replace("-", ".")
-#        ht = cells[3].find(text=True)
-        wt = cells[7].find(text=True)
-        print wt
+        #ht = cells[3]
+        #wt = cells[2]
+        #print name
 
-        f.writerow([name, ht, wt])
+        f.writerow([team, name, ht, wt])
 
 
 getStatsFromWeb(page1);
