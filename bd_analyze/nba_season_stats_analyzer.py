140ce1c528b9ad195480aca9a310fb5e8410d158
kukumaluer <yoho.song@gmail.com>
2014-03-21 23:13:03 -0700

    Updating

--- bd_analyze/nba_season_stats_analyzer.py
+++ bd_analyze/nba_season_stats_analyzer.py
@@ -13,22 +13,22 @@ import random
 
 
 # Argument parsing
-parser = argparse.ArgumentParser(description='Jumpball analyze')
-parser.add_argument('-s', '--season', action='store', help='Season in year', dest="year_season", required=True)
-args = parser.parse_args()
-
-season = args.year_season
+#parser = argparse.ArgumentParser(description='Jumpball analyze')
+#parser.add_argument('-s', '--season', action='store', help='Season in year', dest="year_season", required=True)
+#parser.add_argument('-n', '--next-season', action='store', help='Season in year', dest="next_year_season", required=True)
+#args = parser.parse_args()
+#
+#season = args.year_season
+#next_season = args.next_year_season
 
 #data_directory = "./nba_data"
-data_directory = ("/Users/Song/Study/CS454/jumpball/bd_collect/nba_data/%s/" % season)
+#data_directory = ("/Users/Song/Study/CS454/jumpball/bd_collect/nba_data/%s/" % season)
 team_stat_path = './nba_data/*.csv'
 team_stat_files = glob.glob(team_stat_path)
 
 data_types = ['Height', 'Weight', 'WL_PERC']
 num_data_types = len(data_types)
 
-data_set=[]
-team_labels_set = []
 def readTeamStats(file_name):
     dtypes = np.dtype({ 'names' : ('team', 'Height', 'Weight', 'WL_PERC'),
                         'formats' : ['S10', np.float, np.float, np.float] })
@@ -52,7 +52,12 @@ def readTeamRecord(file_name):
 
 
 # Iterate through each NBA team stats file and output find the mean for each teams weight and height
-def analyzeTeamStats():
+def analyzeTeamStats(season):
+
+    data_set=[]
+    team_labels_set = []
+    
+    data_directory = ("/Users/Song/Study/CS454/jumpball/bd_collect/nba_data/%s/" % season)
 
     for root, dirs, files in os.walk(data_directory):
         for f in files:
@@ -69,16 +74,25 @@ def analyzeTeamStats():
                 data_set.append(data)
                 team_labels_set.append(team)
     #print data_set
+    #print "--------------"
     return data_set,team_labels_set
 
-trainData, teamData = analyzeTeamStats()
+season = "2011"
+next_year_season = "2012"
+trainData_1, teamData_1 = analyzeTeamStats(season)
+trainData_2, teamData_2 = analyzeTeamStats(next_year_season)
+print teamData_2
 
-trainData_array = np.array(trainData)
+trainData_array = np.array(trainData_1)
+newData_array = np.array(trainData_2)
+#print trainData_array
+#print newData_array
+#print newData_array[0]
 
 #print trainData_array
 
 labels = np.array(np.arange(30))
-for label, x, y in zip(teamData,trainData_array[:,0],trainData_array[:,1]):
+for label, x, y in zip(teamData_1,trainData_array[:,0],trainData_array[:,1]):
     pyplot.annotate(label,xy =(x,y), xytext=(-20,20),textcoords = 'offset points', 
         ha ='right', va = 'bottom',bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', 
         alpha = 0.5),arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
@@ -88,7 +102,24 @@ for i in range(0,30):
     color = '#%02X%02X%02X' % (r(),r(),r())
     team_data = trainData_array[labels.ravel()==i]
     pyplot.scatter(team_data[:,0],team_data[:,1],marker = 'o',s = team_data[:,2]*500,c = color,cmap = pyplot.get_cmap('Spectral'))
-    #for label, x, y in zip(labels)
+
+for i in range(0,1):
+    new_team_data = newData_array[labels.ravel()==i]
+    pyplot.scatter(new_team_data[:,0],new_team_data[:,1],marker = '^',s = new_team_data[:,2]*1000,c = color,cmap = pyplot.get_cmap('Spectral'))
+
+for label, x, y in zip(teamData_2[0:1],newData_array[:,0],newData_array[:,1]):
+    pyplot.annotate(label,xy =(x,y), xytext=(-20,20),textcoords = 'offset points',
+        ha ='right', va = 'bottom',bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow',
+        alpha = 0.5),arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+
+knn = cv2.KNearest()
+knn.train(trainData_array,labels)
+ret, results, neighbours ,dist = knn.find_nearest(new_team_data, 3)
+
+print "result: ", results,"\n"
+print "neighbours: ", neighbours,"\n"
+print "distance: ", dist
+
 
 pyplot.show()
 
