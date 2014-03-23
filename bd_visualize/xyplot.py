3b3584ba70176acf58b1dee3909ca109d0d03414
Aidan Wong <aidan.wong@MBA-107-OP-31.local>
2014-03-22 18:12:55 -0700

    adding

---
+++ bd_visualize/xyplot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def xyplotter(data, figure)
+
+# define some data
+x = data
+y = [20, 21, 20.5, 20.8]
+
+
+# plot data
+plt.plot(x,y)
+
+#plt.show()
---
+++ bd_visualize/xyplot_example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def xyplotter(arguments, data, figure)
+
+# define some data
+#x = [1,2,3,4]
+#y = [20, 21, 20.5, 20.8]
+
+
+# plot data
+#plt.plot(x,y)
+
+plt.show()
